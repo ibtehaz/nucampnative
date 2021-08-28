ea870621f4500c275ae11999de53b1420ed00034
Ibtehaz Utsay <ibtehaz.utsay@gmail.com>
2021-08-27 19:42:41 -0700

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
+import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 import { CAMPSITES } from '../shared/campsites';
 
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    }, 
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const AppNavigator = createAppContainer(DirectoryNavigator);
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -18,16 +42,13 @@ class Main extends Component {
 
     render() {
         return (
-            <View style={{flex: 1}}>
-                <Directory
-                    campsites={this.state.campsites}
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id === this.state.selectedCampsite)[0]}
-                />
-            </View> 
+            <View
+            style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+        }}>
+            <AppNavigator />
+        </View>
         );
     }
 }
